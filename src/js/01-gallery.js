b2e8322f32726b80b1747b3088a4e9526ff8c51f
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-06-17 22:18:27 +0200

    добавляет библиотеку simplelightbox через npm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const imageCardsMarkup = createImageCardsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', imageCardsMarkup);
+
+function createImageCardsMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href=${original}>
+          <img class="gallery__image" src=${preview} alt=${description} />
+        </a>`
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
