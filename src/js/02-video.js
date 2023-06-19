b9be4510bacac66ec0d403254a1a7b7746e6249d
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-06-19 14:39:46 +0200

    8.2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
 
+const PLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate({ seconds }) {
+  localStorage.setItem(PLAYER_CURRENT_TIME, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(PLAYER_CURRENT_TIME));
