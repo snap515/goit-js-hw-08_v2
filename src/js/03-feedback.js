67f16c3fc1129b394d9d50f5bf2ddacf4a821f89
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-06-21 13:50:30 +0200

    finished 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,6 +3,9 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 
 const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input[name = "email"]');
+const textAreaEl = document.querySelector('textarea[name="message"]');
+
 const formData = {};
 
 formEl.addEventListener('submit', onFormSubmit);
@@ -13,18 +16,32 @@ onSavedInput();
 function onFormSubmit(e) {
   e.preventDefault();
   console.log('Отправляем форму:', formData);
+
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
+
 function onInput(e) {
   formData[e.target.name] = e.target.value;
-
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  console.log(formData);
+
+  updateFormData();
 }
 
 function onSavedInput() {
-  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (savedMessage) {
-    console.dir(formEl);
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData && savedData.email !== '') {
+    inputEl.value = savedData.email;
   }
+
+  if (savedData && savedData.message !== '') {
+    textAreaEl.value = savedData.message;
+  }
+}
+
+function updateFormData() {
+  formData.email = inputEl.value;
+  formData.message = textAreaEl.value;
 }
