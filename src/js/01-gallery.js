1302e8ee69b5c8dfed89de21050afb7c697d84c6
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-10-28 16:18:53 +0200

    removes console.log

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,8 +4,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryEl = document.querySelector('.gallery');
 const imageCardsMarkup = createImageCardsMarkup(galleryItems);
 
