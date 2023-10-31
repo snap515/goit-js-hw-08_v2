9ea09f703ed7bb5b8625bcbd0d66433da3127cbc
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-10-31 19:39:38 +0100

    фиксит 3 задачу. добавляет функцию onLoad, фиксит ошибки

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,44 +3,37 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 
 const formEl = document.querySelector('.feedback-form');
-const inputEl = document.querySelector('input[name = "email"]');
-const textAreaEl = document.querySelector('textarea[name="message"]');
 
-const formData = {};
+let formData = {};
 
 formEl.addEventListener('submit', onFormSubmit);
 formEl.addEventListener('input', throttle(onInput, 500));
 
-onSavedInput();
+onLoad();
+
+function onLoad() {
+  try {
+    const data = localStorage.getItem(STORAGE_KEY);
+    if (!data) return;
+    formData = JSON.parse(data);
+    Object.entries(formData).forEach(([key, val]) => {
+      formEl.elements[key].value = val;
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
 function onFormSubmit(e) {
   e.preventDefault();
   console.log('Отправляем форму:', formData);
 
+  formData = {};
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
-}
-
-function onInput(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-
-  updateFormData();
-}
-
-function onSavedInput() {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  if (savedData && savedData.email !== '') {
-    inputEl.value = savedData.email;
-  }
-
-  if (savedData && savedData.message !== '') {
-    textAreaEl.value = savedData.message;
-  }
-}
-
-function updateFormData() {
-  formData.email = inputEl.value;
-  formData.message = textAreaEl.value;
 }
